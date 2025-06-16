e9ee39bd4ee7cee7a6c66997bda2829e4d04f294
gudziv <gudz_iv@mail.ru>
2025-06-16 18:37:26 +0300

    Исправление ошибок задания Знакомство с PageObject. Практика работы с Page Object

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -7,21 +7,21 @@ class RegistrationPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.field_input_email = page.get_by_test_id('registration-form-email-input').locator('input')
-        self.field_input_username = page.get_by_test_id('registration-form-username-input').locator('input')
-        self.field_input_password = page.get_by_test_id('registration-form-password-input').locator('input')
+        self.email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        self.username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        self.password_input = page.get_by_test_id('registration-form-password-input').locator('input')
         self.registration_button = page.get_by_test_id('registration-page-registration-button')
 
-    def fill_registration_form(self, email: 'str', username: 'str', password: 'str'):
-        self.field_input_email.clear()
-        self.field_input_username.clear()
-        self.field_input_password.clear()
-        self.field_input_email.fill(email)
-        self.field_input_username.fill(username)
-        self.field_input_password.fill(password)
-        expect(self.field_input_email).to_have_value(email)
-        expect(self.field_input_username).to_have_value(username)
-        expect(self.field_input_password).to_have_value(password)
+    def fill_registration_form(self, email: str, username: str, password: str):
+        self.email_input.clear()
+        self.username_input.clear()
+        self.password_input.clear()
+        self.email_input.fill(email)
+        self.username_input.fill(username)
+        self.password_input.fill(password)
+        expect(self.email_input).to_have_value(email)
+        expect(self.username_input).to_have_value(username)
+        expect(self.password_input).to_have_value(password)
 
     def click_registration_button(self):
         self.registration_button.click()
