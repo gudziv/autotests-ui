49a6617a6f7db9cb1491247e515452f3263ab94c
gudziv <gudz_iv@mail.ru>
2025-06-23 20:55:29 +0300

    Знакомство с PageComponent

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -22,6 +22,10 @@ def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
 @pytest.fixture()
+def dashboard_page_with_state(chromium_page_with_state: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page_with_state)
+
+@pytest.fixture()
 def course_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(chromium_page_with_state)
 
