723e124e84eafb37c85aa71908f1ef5969658ba2
gudziv <gudz_iv@mail.ru>
2025-08-23 20:36:13 +0300

    Практические применение PageFactory в автотестах

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -3,7 +3,7 @@ from components.navigation.sidebar_component import SidebarComponent
 from components.dashboard.dashboard_toolbar_view_component import DashboardToolbarViewComponent
 from components.charts.chart_view_component import ChartViewComponent
 from pages.base_page import BasePage
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 
 class DashboardPage(BasePage):
@@ -16,7 +16,28 @@ class DashboardPage(BasePage):
 
         self.dashboard = DashboardToolbarViewComponent(page)
 
-        self.scores_chart_view = ChartViewComponent(page, "scores", "scatter")
-        self.courses_chart_view = ChartViewComponent(page, "courses", "pie")
-        self.students_chart_view = ChartViewComponent(page, "students", "bar")
-        self.activities_chart_view = ChartViewComponent(page, "activities", "line")
+        self.scores_chart = ChartViewComponent(page, "scores", "scatter")
+        self.courses_chart= ChartViewComponent(page, "courses", "pie")
+        self.students_chart = ChartViewComponent(page, "students", "bar")
+        self.activities_chart = ChartViewComponent(page, "activities", "line")
+
+        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+
+    def check_visible_dashboard_title(self):
+        expect(self.dashboard_title).to_be_visible()
+        expect(self.dashboard_title).to_have_text('Dashboard')
+
+
+    def check_visible_students_chart(self):
+        self.students_chart.check_visible('Students')
+
+    def check_visible_activities_chart(self):
+        self.activities_chart.check_visible('Activities')
+
+    def check_visible_courses_chart(self):
+        self.courses_chart.check_visible('Courses')
+
+    def check_visible_scores_chart(self):
+        self.scores_chart.check_visible('Scores')
+
+
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -7,12 +7,10 @@ from pages.dashboard_page import DashboardPage
 @pytest.mark.regression
 def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
-
-    dashboard_page_with_state.sidebar.check_visible()
-
     dashboard_page_with_state.navbar.check_visible('username')
 
-    dashboard_page_with_state.scores_chart_view.check_visible(title='Scores', identifier='Scores')
-    dashboard_page_with_state.courses_chart_view.check_visible(title='Courses', identifier='Courses')
-    dashboard_page_with_state.students_chart_view.check_visible(title='Students', identifier='Students')
-    dashboard_page_with_state.activities_chart_view.check_visible(title='Activities', identifier='Activities')
+    dashboard_page_with_state.check_visible_dashboard_title()
+    dashboard_page_with_state.check_visible_scores_chart()
+    dashboard_page_with_state.check_visible_courses_chart()
+    dashboard_page_with_state.check_visible_students_chart()
+    dashboard_page_with_state.check_visible_activities_chart()
