64b1c047eb700649d52c348a4320194bb11c2bb9
gudziv <gudz_iv@mail.ru>
2025-08-24 18:58:52 +0300

    Практические применение PageFactory в автотестах. Практикуемся в применении Page Factory

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -3,7 +3,7 @@ from  components.navigation.navbar_component import NavbarComponent
 from components.views.empty_view_component import EmptyViewComponent
 from components.views.image_upload_widget_component import ImageUploadWidgetComponent
 from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 from components.courses.create_course_form_component import CreateCourseFormComponent
 from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
 from components.courses.create_course_exercises_toolbar_view_component import CreateCourseExercisesToolbarViewComponent
@@ -13,13 +13,11 @@ class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
 
-
         self.navbar = NavbarComponent(page)
 
         self.create_course_form = CreateCourseExerciseFormComponent(page)
 
         self.image_upload_widget = ImageUploadWidgetComponent(page, identifier='create-course-preview')
-
 
         self.toolbar_courses = CreateCourseToolbarViewComponent(page)
 
@@ -36,10 +34,10 @@ class CreateCoursePage(BasePage):
         self.toolbar_courses.create_course_button.click()
 
     def check_visible_create_course_button(self):
-        expect(self.toolbar_courses.create_course_button).to_be_visible()
+        self.toolbar_courses.create_course_button.check_visible()
 
     def check_disabled_create_course_button(self):
-        expect(self.toolbar_courses.create_course_button).to_be_disabled()
+        self.toolbar_courses.create_course_button.check_disabled()
 
     def check_visible_create_course_form(
             self,
@@ -62,7 +60,7 @@ class CreateCoursePage(BasePage):
         self.course_form.fill(title, estimated_time, description, max_score, min_score)
 
     def check_visible_exercises_title(self):
-        expect(self.exercises_toolbar.title).to_be_visible()
+        self.exercises_toolbar.title.check_visible()
 
     def click_create_exercises_button(self):
         self.exercises_toolbar.click_create_exercise_button()
